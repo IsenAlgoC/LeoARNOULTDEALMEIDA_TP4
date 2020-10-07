0fea29e19d8b3f2a91e78909ab64bba23f54cfe2
Leo-ADA <71765962+Leo-ADA@users.noreply.github.com>
2020-10-07 15:25:20 +0200

    Exo 3

--- LeoARNOULTDEALMEIDA_TP4/Exo3/main.c
+++ LeoARNOULTDEALMEIDA_TP4/Exo3/main.c
@@ -2,8 +2,14 @@
 #include <stdio.h>
 
 int main() {
-	char nom[], prenom[], sexe;
-
-
-
+	char nom[20], prenom[20], sexe;
+	printf("Nom : ");
+	scanf_s("%s", nom, (unsigned)_countof(nom));
+	printf("Prenom : ");
+	scanf_s("%s", prenom, (unsigned)_countof(prenom));
+	printf("Sexe : ");
+	sexe = _getch();
+	if (sexe == 'H'){printf("Monsieur ");}
+	else {printf("Madame ");}
+	printf("%s %s",prenom,nom);
 }
