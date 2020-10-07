9f57136d85f2e3cf746dd90674c1d2b58710a78b
Leo-ADA <71765962+Leo-ADA@users.noreply.github.com>
2020-10-07 17:47:13 +0200

    Fin tp

--- LeoARNOULTDEALMEIDA_TP4/Exo4/main.c
+++ LeoARNOULTDEALMEIDA_TP4/Exo4/main.c
@@ -8,20 +8,15 @@ int main() {
 	printf("Mot : ");
 	scanf_s("%s", mot, (unsigned)_countof(mot));
 	for (int i=0; i < 20; i++) { mot[i] = tolower(mot[i]);}
+	printf("Ce mot est ");
 	for (int i = 0; i < 20; i++) {
-        switch ((char)mot[i]) {
-            case 'à': mot[i] = 'a'; break;
-            case 'è': mot[i] = 'e'; break;
-            case 'é': mot[i] = 'e'; break;
-            case 'ì': mot[i] = 'i'; break;
-            case 'ò': mot[i] = 'o'; break;
-            case 'ù': mot[i] = 'u'; break;
-            default:    break;
-        }
-    }
-	//strcpy(mot,);
+		if ((mot[i] == 'é' ) || (mot[i] == 'è')) { mot[i] = 'e'; }
+		else if (mot[i] == 'à') { mot[i] = 'a'; }
+		else if (mot[i] == 'ù') { mot[i] = 'u'; }
+	}
+	for (int i = 0; i < strlen(mot); i++) {
+		if (mot[i] == mot[strlen(mot) - i]) { printf("pas "); break; }
+	}
+	printf("un palindrome");
 	puts(mot);
-
-
-
 }
