35155b351a51292323a6a8d97ed8011f07609370
Leo-ADA <71765962+Leo-ADA@users.noreply.github.com>
2020-10-07 14:10:58 +0200

    Exo 1

--- LeoARNOULTDEALMEIDA_TP4/TP4/main.c
+++ LeoARNOULTDEALMEIDA_TP4/TP4/main.c
@@ -1,6 +1,22 @@
 #include "tp4.h"
+#include <stdlib.h>
+#include <stdio.h>
 int main() {
 	HEURE HeureDebut, HeureFin, Duree;
+	HeureDebut.heure = 12;
+	HeureDebut.minute = 30;
+	Duree.heure = 0;
+	Duree.minute = 45;
+	
+	HeureFin.minute = HeureDebut.minute + Duree.minute;
+	HeureFin.heure = HeureDebut.heure + Duree.heure;
+	if (HeureFin.minute / 60 >= 1) {
+		HeureFin.heure += (HeureFin.minute - (HeureFin.minute % 60)) / 60 ;
+		HeureFin.minute = HeureFin.minute % 60;
+	}
+	printf("Depart %02d:%02d Duree %02d:%02d Arrivee %02d:%02d", HeureDebut.heure, HeureDebut.minute, Duree.heure, Duree.minute, HeureFin.heure, HeureFin.minute);
+
+
 
 
 }
